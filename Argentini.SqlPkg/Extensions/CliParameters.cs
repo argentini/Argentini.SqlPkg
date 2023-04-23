0e69f10f063fb1ba27fbb1eaf1a0bc053ab5e0f2
Michael Argentini <michael@argentini.us>
2023-04-22 23:10:05 -0400

    Code comments

--- Argentini.SqlPkg/Extensions/CliParameters.cs
+++ Argentini.SqlPkg/Extensions/CliParameters.cs
@@ -2,6 +2,14 @@ namespace Argentini.SqlPkg.Extensions;
 
 public static class CliParameters
 {
+    /// <summary>
+    /// Get a CLI argument value, or an emtpy string if not found.
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <param name="startsWith"></param>
+    /// <param name="startsWithAbbrev"></param>
+    /// <param name="delimiter"></param>
+    /// <returns></returns>
     public static string GetArgumentValue(this IEnumerable<string>? arguments, string startsWith, string startsWithAbbrev, char delimiter)
     {
         if (arguments == null)
@@ -25,6 +33,13 @@ public static class CliParameters
         return string.Empty;
     }
 
+    /// <summary>
+    /// Set a default CLI argument value if it doesn't exist.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <param name="argumentPrefix"></param>
+    /// <param name="appendValue"></param>
+    /// <returns></returns>
     public static string[] SetDefault(this string[] args, string argumentPrefix, string appendValue)
     {
         if (args.Any(a => a.StartsWith(argumentPrefix, StringComparison.CurrentCultureIgnoreCase)))

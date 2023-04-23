590fd54f3cccae56a6d776037c6f92ca521ed0da
Michael Argentini <michael@argentini.us>
2023-04-22 23:23:39 -0400

    Comment removal

--- Argentini.SqlPkg/Program.cs
+++ Argentini.SqlPkg/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text;
 using Argentini.SqlPkg.Extensions;
 using CliWrap;
